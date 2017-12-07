219222dede6aab8c3dc04380a5ec3c601752c758
Johnathan <you@example.com>
2017-12-07 10:34:37 -0500

    baby steps to reproduce media files inside C folder

---
+++ reproduceMedia.js
@@ -0,0 +1,11 @@
+var downloadState = document.getElementById('download');
+var mediaContainer = document.getElementById('content');
+
+downloadState.addEventListener('click', function() {
+    mediaContainer.classList.add('content');
+    /*console.log('Hello World, enjoy it!');*/
+});
+
+function readMediaFolderContent() {
+    
+}
