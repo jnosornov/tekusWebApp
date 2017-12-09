0080f0f54f60c0d5cab21012307214fe239d243d
Johnathan <you@example.com>
2017-12-08 23:06:25 -0500

    some refactoring

--- playground/timeCounter.js
+++ playground/timeCounter.js
@@ -1,7 +1,41 @@
-function timeCounter(time) {
+//import { clearTimeout } from "timers";
+
+/*function timeCounter(time) {
     setTimeout(function() {
         console.log('Hello World');
     }, time);
 }
 
-timeCounter(5000);
+timeCounter(5000);*/
+
+//this is a timer that runs an amount of time for each item passed
+
+var t = 0;
+var activityTimeOut;
+var timeElapsed = 0;
+var timer_is_on = 0;
+
+// time counter, with timeOut step time
+function timedCount(activityTimeOut) {
+    t = setTimeout(function(){ timedCount(activityTimeOut); }, 1000);
+    timeElapsed = timeElapsed + 1;
+    /*console.log('activity time out: ' + activityTimeOut);
+    console.log('time elapsed: ' + timeElapsed);*/
+    if(timeElapsed === activityTimeOut) {
+        stopCount();
+    }
+}
+
+function startCount(timeOut) {
+    if(!timer_is_on) {
+        timer_is_on = 1;
+        timedCount(timeOut);
+    }
+}
+
+function stopCount() {
+    clearTimeout(t);
+    console.log('The timer has finished');
+    timeElapsed = 0;
+    timer_is_on = 0;
+}
--- reproduceMedia.js
+++ reproduceMedia.js
@@ -1,49 +1,157 @@
-//Selectors
-var currentVideoSelector = document.querySelector('.insertedVideo');
-var playIconSelector = document.getElementById('play_media');
-var mediaSesionSelector = document.querySelector('.media');
 
-var htmlVideo = "<video class='insertedVideo' controls autoplay><source src='C:/Media/%video%' type='video/mp4'></video>";
-var htmlImage = "<img class='insertedImage' src='C:/Media/%image%'>";
-//event to start playing media content inside Media folder
+// play media module
+var reproduceMedia = (function(){
+    
+    // selectors
+    var currentVideoSelector = '.insertedVideo';
+    var playIconSelector = 'play_media';
+    var mediaSesionSelector = '.media';
 
-var mediaNames = ['Tekus_BG1.jpg','Arkbox.mp4','Tekus_BG2.jpg','Cronometro.mp4'];
-var mediaTimes = [5,-1,10,10];
+    // elements
+    var currentVideoElement = document.querySelector(currentVideoSelector);
+    var playIconElement = document.getElementById(playIconSelector);
+    var mediaSesionElement = document.querySelector(mediaSesionSelector);
 
-playIconSelector.addEventListener('click', function() {
-    var mediaContainerSelector = document.createElement('div');
-    mediaSesionSelector.appendChild(mediaContainerSelector).classList.add('content');
+    // variables
+    // incoming data from backend
+    var mediaNames = ['Tekus_BG1.jpg','Arkbox.mp4','Tekus_BG2.jpg','Cronometro.mp4'];
+    var mediaTimes = [5,-1,10,10];
+    // html to add video or image
+    var htmlVideo = "<video class='insertedVideo' controls autoplay><source src='C:/Media/%video%' type='video/mp4'></video>";
+    var htmlImage = "<img class='insertedImage' src='C:/Media/%image%'>";
 
-    //var mediaContentSelector = document.querySelector('.content');
-    var newHtmlVideo, newHtmlImage;
-    var mediaContentSelector = document.querySelector('.content');
+    // function to create a container for media to be add
+    var mediaContainer = function() {
+        // create element
+        var mediaContainerTag = 'div';
+        var mediaContainerElement = document.createElement(mediaContainerTag);
 
+        // add element to DOM tree
+        mediaSesionElement.appendChild(mediaContainerElement).classList.add('content');
 
-    for (var i=0; i < mediaNames.length; i++) {
-        console.log('This has been repeated: ' + i + ' times');
-        if(mediaContentSelector.hasChildNodes()) {
-            mediaContentSelector.removeChild(mediaContentSelector.firstChild);
-        } 
-        playStoredMedia('Cronometro.mp4');
-        setTimeout(() => {
-            mediaContentSelector.removeChild(mediaContentSelector.firstChild);
-            playStoredMedia('Arkbox.mp4');
-        }, 5000);
+        // element
+        var mediaContentSelector = '.content';
+        var mediaContentElement = document.querySelector(mediaContentSelector);
+
+        return mediaContentElement;
     }
     
-    function playStoredMedia(name) {
-        var loadMedia;
-        if(name.includes('jpg')) {
+    // function that adds media to the media container
+    var addMedia = function(name, mediaContent) {
+        var loadMedia, newHtmlImage, newHtmlVideo;
+        var isAnImage = name.includes('.jpg');
+        if(isAnImage) {
             newHtmlImage = htmlImage.replace('%image%', name);
             loadMedia = newHtmlImage;
         } else {
             newHtmlVideo = htmlVideo.replace('%video%', name);
             loadMedia = newHtmlVideo;
         }
-    
-        mediaContentSelector.insertAdjacentHTML('afterbegin', loadMedia); 
+        mediaContent.insertAdjacentHTML('afterbegin', loadMedia); 
     }
+
+    // function to start a timer
+    /*
+    var startCount = function(timeOut, timer_is_on) {
+        if(!timer_is_on) {
+            timer_is_on = 1;
+            timedCount(timeOut);
+        }
+    }
+
+    // function to stop a timer
+    var stopCount = function(currentMedia) {
+        console.log(currentMedia);
+        currentMedia = currentMedia + 1;
+        clearTimeout(t);
+        timeElapsed = 0;
+        timer_is_on = 0;
+        mediaContentElement.removeChild(mediaContentElement.firstChild);
+        playMediaInRow(currentMedia);
+    }
+
+    // function to count time
+    var timedCount = function(activityTimeOut) {
+        t = setTimeout(function(){ timedCount(activityTimeOut); }, 1000);
+        timeElapsed = timeElapsed + 1;
+        /*console.log('activity time out: ' + activityTimeOut);
+        console.log('time elapsed: ' + timeElapsed);
+        if(timeElapsed === activityTimeOut) {
+            stopCount(currentMedia);
+        }
+    }
+    */
+
+    // time counter variables
+    var t = 0;
+    var activityTimeOut;
+    var timeElapsed = 0;
+    var timer_is_on = 0;
+    var currentMedia = 0;
+
+    var playMedia = function() {
+
+        var mediaContentElement = mediaContainer();
+        addMedia(mediaNames[0], mediaContentElement);
+        startCount(10);
+
+        // start counter function
+        function startCount(timeOut) {
+            if(!timer_is_on) {
+                timer_is_on = 1;
+                timedCount(timeOut);
+            }
+        }
+
+        // time counter, with timeOut step time
+        function timedCount(mediaTimeOut) {
+            t = setTimeout(function(){ timedCount(mediaTimeOut); }, 1000);
+            timeElapsed = timeElapsed + 1;
+            if(timeElapsed === mediaTimeOut) stopCount(currentMedia);
+        }
+        
+        // stop counter function
+        function stopCount(currentMedia) {
+            //console.log(currentMedia);
+            currentMedia = currentMedia + 1;
+            clearTimeout(t);
+            timeElapsed = 0;
+            timer_is_on = 0;
+            mediaContentElement.removeChild(mediaContentElement.firstChild);
+            addMedia(mediaNames[currentMedia], mediaContentElement);
+            startCount(10);
+            //playMediaInRow(currentMedia);
+        }
+
+        // play media constinously, some bugs here, current Media no changing
+        /*function playMediaInRow(currentMedia) {
+            if(currentMedia > 0 && currentMedia < 4) {
+                addMedia(mediaNames[currentMedia], mediaContentElement);
+                startCount(10);
+            }
+        }*/
+    }
+
+    var init = function() {
+        // event listener that triggers media reproduction
+        playIconElement.addEventListener('click', playMedia);
+    };
+
+    return {
+        init: init
+    }
+
+}());
+
+document.addEventListener('DOMContentLoaded', function() {
+    reproduceMedia.init();
 });
+
+
+
+
+
+
 
 
 
@@ -52,6 +160,8 @@ playIconSelector.addEventListener('click', function() {
 //backend returns media available (to make proves read it from Media folder)
 
 //we compare the data with local data
+
+
 
 //if data complete reproduce it
 
